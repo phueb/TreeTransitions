fix: calculation of singular values from in-out correlation matrix

analysis/get_singular_values.py
@@ -10,46 +10,36 @@ from ludwigcluster.utils import list_all_param2vals
 
 
 # fig
-PLOT_NUM_SVS = 8
+PLOT_NUM_SVS = 64
 LEG_FONTSIZE = 16
 AX_FONTSIZE = 16
 FIGSIZE = (8, 8)
 DPI = None
 
-# in-out correlation matrix
-BINARY = False
-
 # toy data
 NUM_CATS = 32
 
-params = ObjectView(list_all_param2vals(Params, update_d={'param_name': 'test', 'job_name': 'test'})[0])
-params.num_probes = 1024
-params.num_seqs = 2 * 10 ** 6
-params.num_cats_list = [NUM_CATS]
-params.truncate_num_cats = NUM_CATS
-params.truncate_list = [1.0, 1.0]  # [1.0, 1.0] is okay
-params.truncate_type = 'probes'
-params.truncate_control = False
-params.num_partitions = 1
-params.mutation_probs = [0.2, 0.2]
+CLIPPING = False
+
+Params.learning_rate = [0.04]
+Params.mutation_prob = [0.5]
+Params.num_seqs = [1 * 10 ** 6]
+Params.truncate_list = [[1.0, 1.0]]
 
 
-toy_data = ToyData(params)
-
-
-def plot_comparison(ys):
+def plot_comparison(ys, params):
     fig, ax = plt.subplots(1, figsize=FIGSIZE, dpi=DPI)
     plt.title('truncate_list={}\ntruncate_control={}\n'
-              'truncate_num_cats={}\ntruncate_type={}\nmutation_probs={}'.format(
+              'truncate_num_cats={}\ntruncate_type={}\nmutation_prob={}'.format(
         params.truncate_list, params.truncate_control,
-        params.truncate_num_cats, params.truncate_type, params.mutation_probs),
+        params.truncate_num_cats, params.truncate_type, params.mutation_prob),
         fontsize=AX_FONTSIZE)
-    ax.set_ylabel('singular value', fontsize=AX_FONTSIZE)
+    ax.set_ylabel('Singular value', fontsize=AX_FONTSIZE)
     ax.set_xlabel('Principal Component', fontsize=AX_FONTSIZE)
     ax.spines['right'].set_visible(False)
     ax.spines['top'].set_visible(False)
     ax.tick_params(axis='both', which='both', top=False, right=False)
-    ax.set_ylim([0, 14])
+    # ax.set_ylim([0, 14])
     # plot
     for n, y in enumerate(ys):
         ax.plot(y, label='partition {}'.format(n + 1), linewidth=2)
@@ -58,34 +48,84 @@ def plot_comparison(ys):
     plt.show()
 
 
-def make_bigram_count_mat(id_seqs_mat):
-    assert id_seqs_mat.shape[1] == 2  # works with bi-grams only
-    print('Making In-Out matrix with shape={}...'.format(id_seqs_mat.shape))
-    res = np.zeros((toy_data.num_vocab, toy_data.num_vocab))
-    for row_id, col_id in zip(id_seqs_mat[:, 0], id_seqs_mat[:, 1]):
-        res[row_id, col_id] += 1
+def make_bigram_count_mat(word_seqs_mat, x_words, y_words):
+    assert word_seqs_mat.shape[1] == 2  # works with bi-grams only
+    print('Making In-Out matrix with shape={}...'.format(word_seqs_mat.shape))
+    num_xws = len(x_words)
+    num_yws = len(y_words)
+    res = np.zeros((num_xws, num_yws))
+    for xw, yw in word_seqs_mat:
+        res[x_words.index(xw), y_words.index(yw)] += 1
     return res
 
 
-# print results
-singular_vals = []
-for id_seq_mat_chunk in np.vsplit(toy_data.id_sequences_mat, params.num_partitions):
-    in_out_corr_mat = make_bigram_count_mat(id_seq_mat_chunk)
-    mat = normalize(in_out_corr_mat, axis=1, norm='l2', copy=False)
-    #
-    pca = PCA(svd_solver='full')  # pca is invariant to transposition
-    pca.fit(mat)
-    # console
-    print('total var={:,}'.format(np.var(mat, ddof=1, axis=0).sum().round(0)))  # total variance
-    for start, end in [(0, 31), (31, 64), (64, 1023)]:
-        print('start={} end={}'.format(start, end))
+def plot_heatmap(mat, ytick_labels, xtick_labels,
+                 figsize=(10, 10), dpi=None, ticklabel_fs=1, title_fs=5):
+    fig, ax = plt.subplots(figsize=figsize, dpi=dpi)
+    plt.title('', fontsize=title_fs)
+    # heatmap
+    print('Plotting heatmap...')
+    ax.imshow(mat,
+              aspect='equal',
+              cmap=plt.get_cmap('jet'),
+              interpolation='nearest')
+    # xticks
+    num_cols = len(mat.T)
+    ax.set_xticks(np.arange(num_cols))
+    ax.xaxis.set_ticklabels(xtick_labels, rotation=90, fontsize=ticklabel_fs)
+    # yticks
+    num_rows = len(mat)
+    ax.set_yticks(np.arange(num_rows))
+    ax.yaxis.set_ticklabels(ytick_labels,   # no need to reverse (because no extent is set)
+                            rotation=0, fontsize=ticklabel_fs)
+    # remove ticklines
+    lines = (ax.xaxis.get_ticklines() +
+             ax.yaxis.get_ticklines())
+    plt.setp(lines, visible=False)
+    plt.show()
 
-        print('var={:,} %var={:.2f} sgl-val={:,}'.format(
-            np.sum(pca.explained_variance_[start:end]).round(0),
-            np.sum(pca.explained_variance_ratio_[start:end]).round(2),
-            np.sum(pca.singular_values_[start:end]).round(0)))
-    print()
-    singular_vals.append(pca.singular_values_[:PLOT_NUM_SVS])
 
-# plot
-plot_comparison(singular_vals)
+for param2val in list_all_param2vals(Params, update_d={'param_name': 'test', 'job_name': 'test'}):
+    params = ObjectView(param2val)
+    for k, v in sorted(params.__dict__.items()):
+        print(k, v)
+    toy_data = ToyData(params, max_ba=False, make_tokens=True)
+
+    # use legals mat as input to PCA
+    singular_vals = []
+    mat = toy_data.legals_mat
+    pca1 = PCA(svd_solver='full')  # pca is invariant to transposition
+    pca1.fit(mat)
+    singular_vals.append(pca1.singular_values_[:PLOT_NUM_SVS])
+    # plot
+    plot_comparison(singular_vals, params)
+
+    # use in_out correlation mat computed on tokens as input to PCA
+    singular_vals = []
+    for word_seq_mat_chunk in np.vsplit(toy_data.word_sequences_mat, params.num_partitions):
+        in_out_corr_mat = make_bigram_count_mat(
+            word_seq_mat_chunk, toy_data.x_words, toy_data.y_words)
+
+        if CLIPPING:
+            mat2 = np.clip(in_out_corr_mat, 0, 1)
+        else:
+            mat2 = in_out_corr_mat
+
+
+
+        pca2 = PCA(svd_solver='full')  # pca is invariant to transposition
+        pca2.fit(mat2)
+
+        # console
+        print('total var={:,}'.format(np.var(mat, ddof=1, axis=0).sum().round(0)))  # total variance
+        for start, end in [(0, 31), (31, 64), (64, 1023)]:
+            print('start={} end={}'.format(start, end))
+
+            print('var={:,} %var={:.2f} sgl-val={:,}'.format(
+                np.sum(pca2.explained_variance_[start:end]).round(0),
+                np.sum(pca2.explained_variance_ratio_[start:end]).round(2),
+                np.sum(pca2.singular_values_[start:end]).round(0)))
+        print()
+        singular_vals.append(pca2.singular_values_[:PLOT_NUM_SVS])
+    # plot
+    plot_comparison(singular_vals, params)

treetransitions/toy_data.py
@@ -63,7 +63,7 @@ class ToyData:
         self.num_vocab = len(self.vocab)  # used by rnn
         self.word2id = {word: n for n, word in enumerate(self.vocab)}
         #
-        self.yw2node0 = self.make_yw2node0()
+        self.yw2node0 = self.make_yw2node0()  # TODO test node is always -1
         self.legals_mat = self.make_legals_mat()
         self.xw2yws = self.make_xw2yws()
         #
@@ -107,6 +107,7 @@ class ToyData:
         res = {}
         for yw in self.y_words:
             node0 = -1 if np.random.binomial(n=1, p=0.5) else 1
+            # node0 = 1    # TODO test
             res[yw] = node0
         return res
 